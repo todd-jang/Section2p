32fdb4e28862b0bba3f59ea8faef7c16a6f08278
GiHoon Jang <102294660+todd-jang@users.noreply.github.com>
2022-10-23 12:52:27 +0900

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -1,8 +1,9 @@
 import csv
 import sys
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
 TEST_SIZE = 0.4
 
@@ -35,7 +36,6 @@ def load_data(filename):
     """
     Load shopping data from a CSV file `filename` and convert into a list of
     evidence lists and a list of labels. Return a tuple (evidence, labels).
-
     evidence should be a list of lists, where each list contains the
     following values, in order:
         - Administrative, an integer
@@ -55,11 +55,48 @@ def load_data(filename):
         - TrafficType, an integer
         - VisitorType, an integer 0 (not returning) or 1 (returning)
         - Weekend, an integer 0 (if false) or 1 (if true)
-
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+
+    evidence = []
+    labels = []
+
+    months = {'Jan': 0,
+              'Feb': 1,
+              'Mar': 2,
+              'Apr': 3,
+              'May': 4,
+              'June': 5,
+              'Jul': 6,
+              'Aug': 7,
+              'Sep': 8,
+              'Oct': 9,
+              'Nov': 10,
+              'Dec': 11}
+
+    # read csv file
+    csv_file = pd.read_csv(filename)
+
+    # prepare labels dataframe
+    labels_df = csv_file['Revenue']
+
+    # prepare evidence dataframe
+    evidence_df = csv_file.drop(columns=['Revenue'])
+
+    # replace month names with numerical values
+    evidence_df = evidence_df.replace(months)
+
+    # replace boolean with 0/1 values
+    evidence_df['VisitorType'] = evidence_df['VisitorType'].apply(lambda x: 1 if x == 'Returning_Visitor' else 0)
+    evidence_df['Weekend'] = evidence_df['Weekend'].apply(lambda x: 1 if x == 'True' else 0)
+    labels_df = labels_df.apply(lambda x: 1 if x is True else 0)
+
+    # convert dataframes to lists
+    evidence_list = evidence_df.values.tolist()
+    labels_list = labels_df.values.tolist()
+
+    return evidence_list, labels_list
 
 
 def train_model(evidence, labels):
@@ -67,25 +104,30 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    neigh.fit(evidence, labels)
+    return neigh
 
 
 def evaluate(labels, predictions):
     """
     Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificity).
-
+    return a tuple (sensitivity, specificty).
     Assume each label is either a 1 (positive) or 0 (negative).
-
     `sensitivity` should be a floating-point value from 0 to 1
     representing the "true positive rate": the proportion of
     actual positive labels that were accurately identified.
-
     `specificity` should be a floating-point value from 0 to 1
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    tn, fp, fn, tp = confusion_matrix(labels, predictions).ravel()
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+
+    return sensitivity, specificity
+
 
 
 if __name__ == "__main__":
